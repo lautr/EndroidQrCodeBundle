a07b1e01bdbd27e657a3f4b22a654fd5cbd70bc5
endroid <info@endroid.nl>
2014-04-05 06:53:47 -0400

    Use generated route in test to ensure correctness

--- Tests/Controller/QrCodeControllerTest.php
+++ Tests/Controller/QrCodeControllerTest.php
@@ -16,7 +16,11 @@ class QrCodeControllerTest extends WebTestCase
     public function testCreateQrCode()
     {
         $client = static::createClient();
-        $client->request('GET', '/qrcode/Life is too short to be generating QR codes.png');
+        $client->request('GET', $client->getContainer()->get('router')->generate('endroid_qrcode', array(
+            'text' => 'Life is too short to be generating QR codes',
+            'extension' => 'png',
+            'size' => 150
+        )));
 
         return $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
